e168239d097972c0d24c6a6d741682013551c97a
Albert Teira Osuna <33811344+alteos98@users.noreply.github.com>
2019-02-19 10:07:07 +0100

    Add files via upload

---
+++ CompresionSinPerdidas/01.1_codificacion_PRACTICA.py
@@ -0,0 +1,128 @@
+# -*- coding: utf-8 -*-
+
+import random
+
+'''
+0. Dada una codificación R, construir un diccionario para codificar m2c y otro para decodificar c2m
+'''
+R = [('a','0'), ('b','11'), ('c','100'), ('d','1010'), ('e','1011')]
+
+# encoding dictionary
+m2c = dict(R)
+
+# decoding dictionary
+c2m = dict([(c,m) for m, c in R])
+
+
+'''
+1. Definir una función Encode(M, m2c) que, dado un mensaje M y un diccionario 
+de codificación m2c, devuelva el mensaje codificado C.
+'''
+
+def Encode(M, m2c):
+    C = ''
+    for x in M:
+        C = C + m2c[x]
+    return C
+
+''' 
+2. Definir una función Decode(C, c2m) que, dado un mensaje codificado C y un diccionario 
+de decodificación c2m, devuelva el mensaje original M.
+'''
+def Decode(C,c2m):
+    M = ''
+    while len(C):
+        aux = C[0]
+        C = C[1:]
+        while aux not in c2m: 
+            aux += C[0]
+            C = C[1:]
+        M = M + c2m[aux]
+    return M
+
+#------------------------------------------------------------------------
+# Ejemplo 1
+#------------------------------------------------------------------------
+
+R = [('a','0'), ('b','11'), ('c','100'), ('d','1010'), ('e','1011')]
+
+# encoding dictionary
+m2c = dict(R)
+
+# decoding dictionary
+c2m = dict([(c,m) for m, c in R])
+
+
+
+
+
+
+
+#------------------------------------------------------------------------
+# Ejemplo 2
+#------------------------------------------------------------------------
+R = [('a','0'), ('b','10'), ('c','110'), ('d','1110'), ('e','1111')]
+
+# encoding dictionary
+m2c = dict(R)
+
+# decoding dictionary
+c2m = dict([(c,m) for m, c in R])
+
+''' 
+3.
+Codificar y decodificar 20 mensajes aleatorios de longitudes también aleatorias.  
+Comprobar si los mensajes decodificados coinciden con los originales.
+'''
+M1 = 'aebcd'
+M2 = 'abc'
+M3 = 'dbbb'
+M4 = 'cdbaabdddcebbb'
+
+#res = Encode(M4,m2c)
+#print(res)
+#res = Decode(res,c2m)
+#print(res)
+
+#------------------------------------------------------------------------
+# Ejemplo 3 
+#------------------------------------------------------------------------
+R = [('a','0'), ('b','01'), ('c','011'), ('d','0111'), ('e','1111')]
+
+# encoding dictionary
+m2c = dict(R)
+
+# decoding dictionary
+c2m = dict([(c,m) for m, c in R])
+
+''' 
+4. Codificar y decodificar los mensajes  'ae' y 'be'. 
+Comprobar si los mensajes decodificados coinciden con los originales.
+'''
+M1 = 'ae'
+M2 = 'be'
+
+res = Encode(M1,m2c)
+print(res)
+res = Decode(res,c2m)
+print(res)
+
+res = Encode(M2,m2c)
+print(res)
+res = Decode(res,c2m)
+print(res)
+
+'''
+¿Por qué da error?
+No sabe diferenciar entre a, b, c o d ya que empiezan todos ellos por 0.
+
+(No es necesario volver a implementar Decode(C, m2c) para que no dé error)
+'''
+
+
+
+  
+
+
+
+
---
+++ CompresionSinPerdidas/01.3_entropia_PRACTICA.py
@@ -0,0 +1,72 @@
+# -*- coding: utf-8 -*-
+"""
+
+"""
+import math
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+'''
+Dada una lista p, decidir si es una distribución de probabilidad (ddp)
+0<=p[i]<=1, sum(p[i])=1.
+'''
+def es_ddp(p,tolerancia=10**(-5)):
+    for x in p:
+        if x > 1 or x < 0:
+            return False
+    if sum(p) == 1:
+        return True
+    else:
+        return False
+
+
+'''
+Dado un código C y una ddp p, hallar la longitud media del código.
+'''
+
+def LongitudMedia(C,p):
+    sum = 0
+    i = 0
+    for x in C:
+        sum += len(x) * p[i]
+        i += 1
+    return sum
+    
+'''
+Dada una ddp p, hallar su entropía.
+'''
+def H1(p):
+    
+
+'''
+Dada una lista de frecuencias n, hallar su entropía.
+'''
+def H2(n):
+    return False
+
+
+
+'''
+Ejemplos
+'''
+C=['001','101','11','0001','000000001','0001','0000000000']
+p=[0.5,0.1,0.1,0.1,0.1,0.1,0]
+n=[5,2,1,1,1]
+
+print(es_ddp(p))
+#print(H1(p))
+#print(H2(n))
+print(LongitudMedia(C,p))
+
+
+
+'''
+Dibujar H(p,1-p)
+'''
+
+
+
+
+
+

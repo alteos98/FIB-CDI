b994cdd2c5c5b87f4c07b1b43698ae62bef1182c
Albert <alteos98@gmail.com>
2019-02-26 19:07:33 +0100

    ya he hecho el generador de códigos prefijos <3

--- CompresionSinPerdidas/01.2_Desigualdad_Kraft-McMillan_PRACTICA.py
+++ CompresionSinPerdidas/01.2_Desigualdad_Kraft-McMillan_PRACTICA.py
@@ -12,7 +12,7 @@ q-ario, decidir si pueden definir un código.
 def  kraft1(L, q=2):
     res = 0
     for x in L:
-        res += 1 / pow(q,x)
+        res += (1 / pow(q,x))
 
     if res <= 1:
         return True
@@ -51,8 +51,30 @@ Dada la lista L de longitudes de las palabras de un
 código q-ario, hallar un código prefijo con palabras 
 con dichas longitudes
 '''
+'''
+Pre: entra una llista de candidats a ser paraules i la q
+Post: hem afegit a 'candidates' totes les paraules amb 1 símbol més (a partir de les que hi havien a 'candidates')
+'''
+def generateCandidates(candidates, q):
+    newCandidates = []
+    for c in candidates:
+        for x in range(q):
+            newCandidates.append(c + str(x))
+    return newCandidates
+
 def Code(L,q=2):
-    return False
+    L.sort()
+    candidates = ['']
+    prefixCode = []
+    actualLength = 0
+    for l in L:
+        while actualLength < l:
+            candidates = generateCandidates(candidates, q)
+            actualLength += 1
+        prefixCode.append(candidates[0])
+        candidates.pop(0)
+    return prefixCode
+
 #%%
 
 '''
@@ -63,15 +85,14 @@ Ejemplos
 L=[2,3,3,3,4,4,4,6]
 q=2
 
-#L = [2,2,3,3,5]
-
 print("\n",sorted(L),' codigo final:',Code(L,q))
 print(kraft1(L,q))
 print(kraft2(L,q))
 print(kraft3(L,max(L)+1,q))
 
 #%%
-q=3
 L=[1,3,5,5,3,5,7,2,2,2]
+q=3
+
 print(sorted(L),' codigo final:',Code(L,q))
 print(kraft1(L,q))

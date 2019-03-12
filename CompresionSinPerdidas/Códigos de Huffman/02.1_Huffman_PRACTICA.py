a5b0b9d10611dfd3ebca792074f48378fd7fc33d
Albert <alteos98@gmail.com>
2019-03-12 08:12:44 +0100

    falta la última pregunta

--- CompresionSinPerdidas/Códigos de Huffman/02.1_Huffman_PRACTICA.py
+++ CompresionSinPerdidas/Códigos de Huffman/02.1_Huffman_PRACTICA.py
@@ -2,7 +2,7 @@
 """
 
 """
-
+import math
 
 
 #%%----------------------------------------------------
@@ -52,12 +52,34 @@ Dada la ddp p=[1/n,..../1/n] con n=2**8, hallar un código de Huffman asociado,
 la entropía de p y la longitud media de código de Huffman hallado.
 '''
 
+'''
+Dada una ddp p, hallar su entropía.
+'''
+def H1(p):
+    sum = 0.0
+    for x in p:
+        if x != 0:
+            sum += x * math.log2(x)
+    return -sum
+
+def LongitudMedia(C,p):
+    sum = 0
+    i = 0
+    for x in C:
+        sum += len(C[x]) * p[i]
+        i += 1
+    return sum
+
 n=2**8
 p=[1/n for _ in range(n)]
-
-
-
-
+d = {}
+for i in range(len(p)):
+    d[chr(i)] = p[i]
+#print(d)
+m2c = Huffman(d)
+print(m2c)
+print("Entropia: ", H1(p))
+print("Longitud media: ", LongitudMedia(m2c,p))
 
 
 #%%----------------------------------------------------
@@ -74,12 +96,6 @@ def tablaFrecuencias(mensaje):
             listapalabras[x] = listapalabras[x] + 1
     return listapalabras
 
-
-
-
-mensaje='La heroica ciudad dormía la siesta. El viento Sur, caliente y perezoso, empujaba las nubes blanquecinas que se rasgaban al correr hacia el Norte. En las calles no había más ruido que el rumor estridente de los remolinos de polvo, trapos, pajas y papeles que iban de arroyo en arroyo, de acera en acera, de esquina en esquina revolando y persiguiéndose, como mariposas que se buscan y huyen y que el aire envuelve en sus pliegues invisibles. Cual turbas de pilluelos, aquellas migajas de la basura, aquellas sobras de todo se juntaban en un montón, parábanse como dormidas un momento y brincaban de nuevo sobresaltadas, dispersándose, trepando unas por las paredes hasta los cristales temblorosos de los faroles, otras hasta los carteles de papel mal pegado a las esquinas, y había pluma que llegaba a un tercer piso, y arenilla que se incrustaba para días, o para años, en la vidriera de un escaparate, agarrada a un plomo. Vetusta, la muy noble y leal ciudad, corte en lejano siglo, hacía la digestión del cocido y de la olla podrida, y descansaba oyendo entre sueños el monótono y familiar zumbido de la campana de coro, que retumbaba allá en lo alto de la esbeltatorre en la Santa Basílica. La torre de la catedral, poema romántico de piedra,delicado himno, de dulces líneas de belleza muda y perenne, era obra del siglo diez y seis, aunque antes comenzada, de estilo gótico, pero, cabe decir, moderado por uninstinto de prudencia y armonía que modificaba las vulgares exageraciones de estaarquitectura. La vista no se fatigaba contemplando horas y horas aquel índice depiedra que señalaba al cielo; no era una de esas torres cuya aguja se quiebra desutil, más flacas que esbeltas, amaneradas, como señoritas cursis que aprietandemasiado el corsé; era maciza sin perder nada de su espiritual grandeza, y hasta sussegundos corredores, elegante balaustrada, subía como fuerte castillo, lanzándosedesde allí en pirámide de ángulo gracioso, inimitable en sus medidas y proporciones.Como haz de músculos y nervios la piedra enroscándose en la piedra trepaba a la altura, haciendo equilibrios de acróbata en el aire; y como prodigio de juegosmalabares, en una punta de caliza se mantenía, cual imantada, una bola grande debronce dorado, y encima otra más pequenya, y sobre ésta una cruz de hierro que acababaen pararrayos.'
-
-print(tablaFrecuencias(mensaje))
 
 """
 [[' ', 381], [',', 46], ['.', 8], [';', 3], ['B', 1], ['C', 2], ['E', 2], ['L', 3], ['N', 1], ['S', 2], ['V', 1], ['a', 267], ['b', 46], ['c', 64], ['d', 104], ['e', 238], ['f', 6], ['g', 23], ['h', 15], ['i', 98], ['j', 7], ['l', 112], ['m', 49], ['n', 111], ['o', 141], ['p', 48], ['q', 25], ['r', 126], ['s', 145], ['t', 68], ['u', 87], ['v', 11], ['x', 1], ['y', 30], ['z', 11], ['á', 11], ['é', 3], ['í', 12], ['ñ', 4], ['ó', 5], ['ú', 1]]
@@ -137,10 +153,10 @@ Ejemplo
 mensaje='La heroica ciudad dormía la siesta. El viento Sur, caliente y perezoso, empujaba las nubes blanquecinas que se rasgaban al correr hacia el Norte. En las calles no había más ruido que el rumor estridente de los remolinos de polvo, trapos, pajas y papeles que iban de arroyo en arroyo, de acera en acera, de esquina en esquina revolando y persiguiéndose, como mariposas que se buscan y huyen y que el aire envuelve en sus pliegues invisibles. Cual turbas de pilluelos, aquellas migajas de la basura, aquellas sobras de todo se juntaban en un montón, parábanse como dormidas un momento y brincaban de nuevo sobresaltadas, dispersándose, trepando unas por las paredes hasta los cristales temblorosos de los faroles, otras hasta los carteles de papel mal pegado a las esquinas, y había pluma que llegaba a un tercer piso, y arenilla que se incrustaba para días, o para años, en la vidriera de un escaparate, agarrada a un plomo. Vetusta, la muy noble y leal ciudad, corte en lejano siglo, hacía la digestión del cocido y de la olla podrida, y descansaba oyendo entre sueños el monótono y familiar zumbido de la campana de coro, que retumbaba allá en lo alto de la esbeltatorre en la Santa Basílica. La torre de la catedral, poema romántico de piedra,delicado himno, de dulces líneas de belleza muda y perenne, era obra del siglo diez y seis, aunque antes comenzada, de estilo gótico, pero, cabe decir, moderado por uninstinto de prudencia y armonía que modificaba las vulgares exageraciones de estaarquitectura. La vista no se fatigaba contemplando horas y horas aquel índice depiedra que señalaba al cielo; no era una de esas torres cuya aguja se quiebra desutil, más flacas que esbeltas, amaneradas, como señoritas cursis que aprietandemasiado el corsé; era maciza sin perder nada de su espiritual grandeza, y hasta sussegundos corredores, elegante balaustrada, subía como fuerte castillo, lanzándosedesde allí en pirámide de ángulo gracioso, inimitable en sus medidas y proporciones.Como haz de músculos y nervios la piedra enroscándose en la piedra trepaba a la altura, haciendo equilibrios de acróbata en el aire; y como prodigio de juegosmalabares, en una punta de caliza se mantenía, cual imantada, una bola grande debronce dorado, y encima otra más pequenya, y sobre ésta una cruz de hierro que acababaen pararrayos.'
 #mensaje = 'aabbcca'
 
-mensaje_codificado, m2c=EncodeHuffman(mensaje)
-print(tablaFrecuencias(mensaje))
-print(m2c)
-#print(mensaje_codificado)
+mensaje_codificado, m2c = EncodeHuffman(mensaje)
+print("Tabla de frecuencias: ", tablaFrecuencias(mensaje))
+print("Codigo asociado: ", m2c)
+#print("Mensaje codificado: ", mensaje_codificado)
 """
 ¡ATENCIÓN: m2c NO ES ÚNICO!
 m2c={'o': '1110', 'v': '11011101', 'c': '11111', 'j': '00001110', ';': '1011011101', 'u': '11010', 'V': '101101110011', 'r': '0010', 'b': '101110', 'á': '11011001', 'E': '1101111111', 'x': '101101110010', 'g': '1101101', 's': '1100', 'S': '1101111110', 'ó': '110111001', 'e': '011', 'B': '11011111000', 'm': '00000', 'l': '0011', ',': '101111', 'z': '11011000', ' ': '100', 'd': '10101', 'p': '101100', 'L': '1011011111', 'í': '10110110', 'C': '1101111101', 'q': '1011010', 'N': '11011111001', '.': '11011110', 'i': '10100', 'a': '010', 'y': '000010', 'ñ': '110111000', 'ú': '10110111000', 't': '11110', 'h': '0000110', 'é': '1011011110', 'n': '0001', 'f': '00001111'}
@@ -157,14 +173,11 @@ Si no tenemos en cuenta la memoria necesaria para almacenar el diccionario,
 lMensaje = len(mensaje)
 lMensajeCodificado = len(mensaje_codificado)
 ratioCompresion = lMensaje / lMensajeCodificado
-print(lMensaje)
-print(lMensajeCodificado)
-print(ratioCompresion)
+print("Longitud del mensaje original: ", lMensaje)
+print("Longitud del mensaje codificado: ", lMensajeCodificado)
+print("Ratio de compresion: ", ratioCompresion)
 
 """
 Si tenemos en cuenta la memoria necesaria para almacenar el diccionario, 
 haz una estimación de la ratio de compresión.
 """
-
-
-

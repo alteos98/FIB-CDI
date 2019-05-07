7c2dbd2887b86338e305dcb22cb01e0e96503ba0
Albert <alteos98@gmail.com>
2019-05-07 09:58:46 +0200

    H_WH funciona perfectly ya

--- CompresionConPerdidas/DWHT/07_DWHT_PRACTICA.py
+++ CompresionConPerdidas/DWHT/07_DWHT_PRACTICA.py
@@ -17,9 +17,69 @@ H_WH(4)=
        [ 0.5, -0.5,  0.5, -0.5]]
 """
 
-def H_WH(N):
+# número de cambios de signo en una fila
+def numeroCambiosSimbolo(row):
+	counter = 0
+	ant = row[0]
+	for x in row:
+		aux = x
+		if ant != aux:
+			counter += 1
+		ant = aux
+	return counter
 
+# dada una matriz m, se ordenan sus filas en función del número
+# de cambios de signo en cada fila
+def ordenarPorCambiosSigno(m):
+	# inicializaciones
+	N = len(m)
+	m_aux = np.zeros((N, N))
 
+	# ordenar
+	for row in m:
+		n_cambiosSimbolo = numeroCambiosSimbolo(row)
+		m_aux[n_cambiosSimbolo] = row
+
+	return m_aux
+
+def H_WH(N_max):
+	# inicializaciones
+	hN_ant = np.array([1])
+	hN_new = np.array([1])
+	N = 2
+
+	while (N <= N_max):
+		# creación nueva matriz
+		hN_new = np.zeros((N, N))
+
+		# replicar la matriz para los 4 cuadrantes (cambiando de signo el último)
+		n_bloque = int(N / 2)
+		for row in range(int(N / n_bloque)):
+			for col in range(int(N / n_bloque)):
+				x_ini = col * n_bloque
+				x_fi = x_ini + n_bloque
+				y_ini = row * n_bloque
+				y_fi = y_ini + n_bloque
+
+				# cambiar el signo del cuarto
+				if (N / n_bloque) - 1 == row and row == col:
+					hN_new[x_ini:x_fi, y_ini:y_fi] = -hN_ant
+				else:
+					hN_new[x_ini:x_fi, y_ini:y_fi] = hN_ant
+
+		# ordenar según los cambios de signo
+		hN_new = ordenarPorCambiosSigno(hN_new)
+
+		# actualizar matriz anterior
+		hN_ant = hN_new
+
+		# tamaño siguiente matriz NxN
+		N = N * 2
+
+	# multiplicar por 1/sqrt(N)
+	hN_new = (1/np.sqrt(N / 2)) * hN_new
+
+	return hN_new
 
 """
 Implementar la DWHT (Discrete Walsh-Hadamard Transform) y su inversa
@@ -42,12 +102,13 @@ dwht_bloque(
              [ -51, -112.5, 146.5,  45]]
 
 """
-def dwht_bloque(p):
+
+#def dwht_bloque(p):
 
 
 
 
-def idwht_bloque(p):
+#def idwht_bloque(p):
 
 
 """
